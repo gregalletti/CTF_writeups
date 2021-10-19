5aa7c0080a5bea018a35f9a7a76dbcbd52a53207
Stefano Bagarin <37816183+stepolimi@users.noreply.github.com>
2021-10-19 11:45:04 +0200

    Update script.py

--- Reply2021/THE PUZZLE/script.py
+++ Reply2021/THE PUZZLE/script.py
@@ -1,25 +1,29 @@
 puz = open("source.txt", "r").read()
 
 puzzle = puz.split("\n")
-''' find the first element of the puzzle up left
-for row in puzzle:
-	words = row.split(" ")
-	found = True
-	for row2 in puzzle:
-		words2 = row2.split(" ")
-		if words[0] == words2[1]:
-			found = False
-			break
-		if words[2] == words2[3]:
-			found = False
-			break
-	break
-	if found:
-		print(row)
-'''
+
+def findCorner():
+	for i,row in enumerate(puzzle):
+		words = row.split(" ")
+		print(i)
+		found = True
+		for row2 in puzzle:
+			words2 = row2.split(" ")
+			if words[0] == words2[1]:
+				found = False
+				break
+			if words[2] == words2[3]:
+				found = False
+				break
+		if found:
+			print(i)
+			return i
+
+corner_index = findCorner()
+
 big_puzzle = [[0]*200]*200
 
-big_puzzle[0][0] = puzzle[13953]
+big_puzzle[0][0] = puzzle[corner]
 
 for i in range(199):
 	for j in range(199):
@@ -28,7 +32,7 @@ for i in range(199):
 			words_against = row.split(" ")
 			if words[3] == words_against[2]:
 				if words_against[4] != "":
-					print(words_against[4])
+					print(words_against[4], end="")
 				big_puzzle[i][j+1] = row
 
 	words = big_puzzle[i][0].split(" ")
@@ -36,7 +40,7 @@ for i in range(199):
 		words_against = row.split(" ")
 		if words[1] == words_against[0]:
 			if words_against[4] != "":
-				print(words_against[4])
+				print(words_against[4], end="")
 			big_puzzle[i+1][0] = row
 
 
